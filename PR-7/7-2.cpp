05c96315453c5d4f29b9710c35b8dcc09dfd36ca
Gautam <114165275+DGJ2627@users.noreply.github.com>
2023-01-19 10:01:09 +0530

    PROJECT

---
+++ PR-7/7-2.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+using namespace std;
+
+class A {
+public :
+    int a;
+
+    public :
+
+      virtual  void setA() {
+
+            cout << "Enter A : ";
+            cin  >> a;
+        }
+
+        virtual void getA() {
+
+            cout <<" A : " << a << endl;
+        }
+};
+
+class B : virtual public A {
+
+    
+
+    public :
+		int b;
+       virtual void setB() {
+
+            cout << "Enter B : ";
+            cin  >> b;
+        }
+
+       virtual void getB() {
+
+            cout <<" B : " << b << endl;
+        }
+};
+
+class C : virtual public A {
+
+   
+
+    public :
+ 		int c;
+       virtual void setC() {
+
+            cout << "Enter C : ";
+            cin  >> c;
+        }
+
+        virtual void getC() {
+
+            cout <<" C : " << c << endl;
+        }
+};
+
+class D : public B , public C {
+
+    int d;
+
+    public :
+
+        void setD() {
+
+            cout << "Enter D : ";
+            cin  >> d;
+        }
+
+         void getD() {
+
+            cout <<" D : " << d << endl << endl;
+            cout << "Sum A+B+C+D = " << a+b+c+d << endl;
+        }
+};
+
+int main() {
+
+    D d1;
+
+    d1.setA();
+    d1.setB();
+    d1.setC();
+    d1.setD();
+
+    d1.getA();
+    d1.getB();
+    d1.getC();
+    d1.getD();
+
+}
